13783f85ad2344175665eb9b7bdc5a77951e74e8
elipmoc <relipmoc101@gmail.com>
2017-05-10 21:59:40 +0900

    キー判定システムの汎用化

--- dxlibex/dxlibex/Base/KeyControl.cs
+++ dxlibex/dxlibex/Base/KeyControl.cs
@@ -7,7 +7,7 @@ using DxLibDLL;
 
 namespace DXEX.Base {
     //キーコード定義
-   public enum KeyCode
+  /* public enum KeyCode
     {
         //方向キー：キャラクターやカーソルの移動
         UP = 0,
@@ -37,15 +37,14 @@ namespace DXEX.Base {
         F2 ,
         //Xキー：メニュー開く
         X ,
-    };
+    };*/
     
 
     //ボタンイベント構造体
    public struct ButtonEvent
     {
-        public delegate void Handler();
         //イベント
-        public event Handler Event;
+        public event Action Event;
         //イベント実行
         public void EventDo() {
             if (Event != null) Event();
@@ -61,17 +60,31 @@ namespace DXEX.Base {
         KEEPDOWN,//押し続けてる時
     }
 
+    class Key
+    {
+        //押された瞬間に呼ばれるイベント
+        public ButtonEvent buttonDown;
+        //離した瞬間に呼ばれるイベント
+        public ButtonEvent buttonUp;
+        //離し続けてる時に呼ばれるイベント
+        public ButtonEvent buttonKeepUp ;
+        //押し続けてる時に呼ばれるイベント
+        public ButtonEvent buttonKeepDown;
+        public int count=0;
+    }
+
     //キー判定をし、イベント、キーの状態を管理するクラス
   public  static class KeyControl
-    {   
+    {
+        static Dictionary<int, Key> keyList=new Dictionary<int, Key>();
         //DXライブラリ用のキーコードと enum KeyCode との互換性を保つ用
-        static int[] dxKeyCode = {
+        /*static int[] dxKeyCode = {
             DX.KEY_INPUT_UP,DX.KEY_INPUT_DOWN,DX.KEY_INPUT_LEFT,DX.KEY_INPUT_RIGHT,
             DX.KEY_INPUT_RETURN,DX.KEY_INPUT_BACK,DX.KEY_INPUT_SPACE,DX.KEY_INPUT_NUMPAD0,
             DX.KEY_INPUT_NUMPAD1,DX.KEY_INPUT_NUMPAD2,DX.KEY_INPUT_NUMPAD3,DX.KEY_INPUT_NUMPAD4,
             DX.KEY_INPUT_NUMPAD5,DX.KEY_INPUT_NUMPAD6,DX.KEY_INPUT_NUMPAD7,DX.KEY_INPUT_NUMPAD8,
-            DX.KEY_INPUT_NUMPAD9,DX.KEY_INPUT_F1,DX.KEY_INPUT_F2,DX.KEY_INPUT_X };
-        //押された瞬間に呼ばれるイベント
+            DX.KEY_INPUT_NUMPAD9,DX.KEY_INPUT_F1,DX.KEY_INPUT_F2,DX.KEY_INPUT_X };*/
+        /*//押された瞬間に呼ばれるイベント
         static ButtonEvent[] buttonDown = new ButtonEvent[dxKeyCode.Length];
         //離した瞬間に呼ばれるイベント
         static ButtonEvent[] buttonUp = new ButtonEvent[dxKeyCode.Length];
@@ -80,66 +93,73 @@ namespace DXEX.Base {
         //押し続けてる時に呼ばれるイベント
         static ButtonEvent[] buttonKeepDown = new ButtonEvent[dxKeyCode.Length];
         //キー入力状態を保持する変数
-        static int[] keyData = new int[dxKeyCode.Length];
+        static int[] keyData = new int[dxKeyCode.Length];*/
         //buttonイベントに関数追加  (ボタンの状態、キーコード、関数)
-        static public void AddButtonEvent(ButtonType bt, KeyCode k, ButtonEvent.Handler h) {
+        static public void AddButtonEvent(ButtonType bt, int keycode, Action func) {
             switch (bt)
             {
                 case ButtonType.UP:
-                    buttonUp[(int)k].Event += h;
+                    keyList[keycode].buttonUp.Event += func;
                     break;
                 case ButtonType.DOWN:
-                    buttonDown[(int)k].Event += h;
+                    keyList[keycode].buttonDown.Event += func;
                     break;
                 case ButtonType.KEEPUP:
-                    buttonKeepUp[(int)k].Event += h;
+                    keyList[keycode].buttonKeepUp.Event += func;
                     break;
                 case ButtonType.KEEPDOWN:
-                    buttonKeepDown[(int)k].Event += h;
+                    keyList[keycode].buttonKeepDown.Event += func;
                     break;
             }
         }
         //buttonイベントから関数削除
-        static public void DeleteButtonEvent(ButtonType bt, KeyCode k, ButtonEvent.Handler h)
+        static public void DeleteButtonEvent(ButtonType bt, int keycode , Action func)
         {
             switch (bt)
             {
                 case ButtonType.UP:
-                    buttonUp[(int)k].Event -= h;
+                    keyList[keycode].buttonUp.Event -= func;
                     break;
                 case ButtonType.DOWN:
-                    buttonDown[(int)k].Event -= h;
+                    keyList[keycode].buttonDown.Event -= func;
                     break;
                 case ButtonType.KEEPUP:
-                    buttonKeepUp[(int)k].Event -= h;
+                    keyList[keycode].buttonKeepUp.Event -= func;
                     break;
                 case ButtonType.KEEPDOWN:
-                    buttonKeepDown[(int)k].Event -= h;
+                    keyList[keycode].buttonKeepDown.Event -= func;
                     break;
             }
         }
-        //特定キーの状態を得る（0：押されてない,1:押した瞬間,それ以外:押されつづけたフレーム数）
-        static public int GiveKey(KeyCode k)
+        //使用するキーを登録
+        static public void ResistKey(int keycode)
         {
-           return keyData[(int)k];
+            if (keyList.ContainsKey(keycode) == true) throw new Exception("既に登録されたキーです");
+            keyList.Add(keycode, new Key());
+        }
+        //特定キーの状態を得る（0：押されてない,1:押した瞬間,それ以外:押されつづけたフレーム数）
+        static public int GiveKey(int keycode)
+        {
+            return keyList[keycode].count;
         }
         //メインループで呼び出すべき処理
         static public void Getkey()
         {
-            for(int i= 0; i<dxKeyCode.Length;i++)
+            foreach(var keyPair in keyList)
             {
-                if (DX.CheckHitKey(dxKeyCode[i]) == 1)
+                if (DX.CheckHitKey(keyPair.Key) == 1)
                 {
-                    keyData[i]++;
-                    if (keyData[i] == 1) buttonDown[i].EventDo();
-                    buttonKeepDown[i].EventDo();
+                    keyPair.Value.count++;
+                    if (keyPair.Value.count == 1) keyPair.Value.buttonDown.EventDo();
+                    keyPair.Value.buttonKeepDown.EventDo();
                 }
                 else {
-                    if (keyData[i] == 0) buttonKeepUp[i].EventDo();
+                    if (keyPair.Value.count == 0) keyPair.Value.buttonKeepUp.EventDo();
                     else
                     {
-                        keyData[i] = 0;
-                        buttonUp[i].EventDo();
+                        keyPair.Value.count = 0;
+                        keyPair.Value.buttonKeepUp.EventDo();
+                        keyPair.Value.buttonUp.EventDo();
                     }
                 }
             }
