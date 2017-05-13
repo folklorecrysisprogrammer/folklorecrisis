8ab325e64b3de436742ed798e81921365593a0d0
a-to-ha <ath3739@gmail.com>
2017-05-13 23:32:29 +0900

    FFT / IFFT の実装(なんか音が変)

--- WindowsFormsApplication1/WindowsFormsApplication1/ShowSound.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/ShowSound.cs
@@ -22,13 +22,15 @@ class ShowSound : Node
     double[] ParamList2 = new double[BufferLength];
     int[] Wave = new int[StackLength];
     int[] Wave2 = new int[StackLength];
+    int[] ZeroWave = new int[StackLength];
+    int[] WaveIm = new int[StackLength];
     int i;
     Letter letter;
 
     public ShowSound()
     {
         // ソフトウェアサウンドで読み込み
-        SoftSoundHandle = DX.LoadSoftSound("for_victory.mp3");
+        SoftSoundHandle = DX.LoadSoftSound("gj.mp3");
         SoftSoundPlayerHandle = DX.MakeSoftSoundPlayer(SoftSoundHandle);
         SoftSoundSampleNum = DX.GetSoftSoundSampleNum(SoftSoundHandle);
 
@@ -56,6 +58,10 @@ class ShowSound : Node
                     DX.ReadSoftSoundData(SoftSoundHandle, OutputSampleNum + i, out Wave[i], out Wave2[i]);
                 }
 
+                // ダミー / 0波を作る
+                for( int i=0; i<Wave.Length; i++)
+                    ZeroWave[i] = 0;
+
                 // エンターキーを押している間だけ，音データを加工する
                 if (KeyControl.GiveKey(KeyCode.ENTER) > 0)
                 {
@@ -74,28 +80,46 @@ class ShowSound : Node
                         Wave[i] = (int)(Math.Sin(d) * 32768);
                         Wave2[i] = (int)(Math.Sin(d) * 32768);
                         Wave2[i] = 0;
-
-                        /*
-                        freq = (int)(freq * 3 / 2); // 周波数(3倍音の半オクターブ下)
-                        d = 2 * Math.PI * freq * i / fs;
-                        Wave[i] += (int)(Math.Sin(d) * 32768);
-                        Wave2[i] += (int)(Math.Sin(d) * 32768);
-
-                        freq = (int)(freq / 3 * 2); // 周波数(1オクターブ上)
-                        d = 2 * Math.PI * freq * i / fs;
-                        Wave[i] += (int)(Math.Sin(d) * 32768);
-                        Wave2[i] += (int)(Math.Sin(d) * 32768);
-                        */
                     }
                 }
 
+                // Waveに対して(左側スピーカ)
                 // FFTによりフーリエ変換を行う
                 // bitsize : ビット数でサイズを指定。( BufferSizeは2の何乗であるか)
-                FFT(Wave, Wave2, out ParamList, out ParamList2, bitsize);
+                // Wave2 : 虚数領域。(普通は0を設定)
+                int[] a = Wave;
+                FFT(Wave, ZeroWave, out ParamList, out ParamList2, bitsize);
 
-                // DXLIBのやつ(動かなくなった)
-                // 現在の再生位置から 4096 サンプルを使用して周波数分布を得る
-                // DX.GetFFTVibrationSoftSound(SoftSoundHandle, -1, OutputSampleNum, BufferLength, out ParamList[0], BufferLength);
+                // 適当にフィルターを噛ませます
+                // (適当なハイパスフィルタ)
+                for (int i=0; i<ParamList.Length/2; i++)
+                {
+                    ParamList[i] /= 2;
+                    ParamList2[i] /= 2;
+                }
+
+                // IFFTで逆フーリエ変換を行う
+                IFFT(ParamList, ParamList2, out Wave, out WaveIm, bitsize, Wave.Length);
+                int[] b = Wave;
+                int[] c = a;
+
+
+                // Wave2に対して(右側スピーカ)
+                // FFTによりフーリエ変換を行う
+                // bitsize : ビット数でサイズを指定。( BufferSizeは2の何乗であるか)
+                // Wave2 : 虚数領域。(普通は0を設定)
+                FFT(Wave2, ZeroWave, out ParamList, out ParamList2, bitsize);
+
+                // 適当にフィルターを噛ませます
+                // (適当なハイパスフィルタ)
+                for (int i = 0; i < ParamList.Length / 2; i++)
+                {
+                    ParamList[i] = 0;
+                    ParamList2[i] = 0;
+                }
+
+                // IFFTで逆フーリエ変換を行う
+                IFFT(ParamList, ParamList2, out Wave2, out WaveIm, bitsize, Wave.Length);
 
                 // 取得した波形サンプルをプレイヤーに出力
                 for (int i = 0; i < StackLength; i++)
@@ -106,10 +130,6 @@ class ShowSound : Node
                 // プレイヤーに出力したサンプルの数をインクリメント
                 OutputSampleNum += StackLength;
             }
-
-            // 現在の再生位置を取得(ソフトサウンドじゃ使えない)
-            // SamplePos = DX.GetCurrentPositionSoundMem(SoundHandle);
-            // letter.Text = "" + SamplePos;
 
             // まだプレイヤーの再生処理を開始していなかったら開始する
             if (DX.CheckStartSoftSoundPlayer(SoftSoundPlayerHandle) == DX.FALSE)
@@ -126,37 +146,15 @@ class ShowSound : Node
         yield break;
     }
 
-
     public override void Draw()
     {
         drawSpectrum();
         // drawWaveform();
     }
 
-
     // Xサンプルの平均を取ってる(飛ばし表示だけにすべきかも)
     private void drawWaveform()
     {
-        /*
-        int x = -1;
-        int ave = 50;
-        for( i =0; i < Channel1.Length/ave; i++)
-        {
-            // 縦線を描画
-
-            
-            //ReadSoftSoundData は8bit量子化のサウンドの場合は -128～127 の値が、
-            //16bit量子化のサウンドの場合が - 32768～32767 の値が Channel1, Channel2 に代入されます
-            float param = 0;
-            for (int j = 0; j < ave; j++)
-            {
-                param += Channel1[i*ave+j];
-            }
-            param /= ave;
-            param /= 32768; // 16bit量子化サウンドを正規化              
-            DX.DrawBox( i , 250 - (int)(param * 200), i+1, 250, DX.GetColor(255, 255, 0), DX.TRUE);
-        }
-        */
     }
 
     private void drawSpectrum()
@@ -178,7 +176,7 @@ class ShowSound : Node
 
                 // 関数から取得できる値を描画に適した値に調整
                 // param = Math.Pow(ParamList[i], 0.1) * 4.0; // DXLIBのやつ
-                int normalize = 1000000; // いくつだろうね.
+                int normalize = 10000; // いくつだろうね.
                 param = ParamList2[i] / normalize;
                 param *= ParamList[i] / normalize; // (実数部と虚数部から大きさを求める…)
                 param = Math.Abs(param);
@@ -189,110 +187,177 @@ class ShowSound : Node
         }
     }
 
-    private void makeText()
-    {
-        // テキストデータの生成
-        letter = new Letter();
-        letter.Color = DX.GetColor(127, 127, 127);
-        letter.LocalPos = new Vect(400, 400);
-        AddChild(letter);
-    }
-
-
-    // フーリエ変換
-    // (拾い物)
-    // URL: https://algorithm.joho.info/signal/csharp-fast-fourier-transform/
-    public static void FFT(
+// 4.3のfft.h(c言語)の内容を元に作成
+public static void FFT(
     int[] inR,
     int[] inI,
     out double[] outputRe,
     out double[] outputIm,
-    int bitSize)
-    {        
-        int dataSize = 1 << bitSize;
-        int[] reverseBitArray = BitScrollArray(dataSize);
-
-        outputRe = new double[dataSize];
-        outputIm = new double[dataSize];
-
-        // 0埋したサイズを調整した波形データを生成する
-        // ( 0埋方法 ) 日本語でおっけー。窓関数使ってないので影響ないと思われ。
-        // http://wiener.seigyo.numazu-ct.ac.jp/CreativeDesign/souzou2005/group2/document/Report_PackData.pdf
-        int sizeInput = inR.Length;
-        int[] inputRe = new int[dataSize];
-        int[] inputIm = new int[dataSize];
-        for( int i= dataSize-1; i>=0; i--)
-        {
-            if((sizeInput - 1 - i) < 0)
-            {
-                inputRe[i] = 0;
-                inputIm[i] = 0;
-                continue;
-            }
-            inputRe[i] = inR[sizeInput - 1 - i];
-            inputIm[i] = inI[sizeInput - 1 - i];
-            int temp = inR[sizeInput - 1 - i];
-        }
-        int abcdemart = inputRe.Length;
-
-        // バタフライ演算のための置き換え
-        for (int i = 0; i < dataSize; i++)
-        {
-            outputRe[i] = inputRe[reverseBitArray[i]];
-            outputIm[i] = inputIm[reverseBitArray[i]];
-        }
-
-        // バタフライ演算
-        for (int stage = 1; stage <= bitSize; stage++)
-        {
-            int butterflyDistance = 1 << stage;
-            int numType = butterflyDistance >> 1;
-            int butterflySize = butterflyDistance >> 1;
-
-            double wRe = 1.0;
-            double wIm = 0.0;
-            double uRe =
-                Math.Cos(Math.PI / butterflySize);
-            double uIm =
-                -Math.Sin(Math.PI / butterflySize);
-
-            for (int type = 0; type < numType; type++)
-            {
-                for (int j = type; j < dataSize; j += butterflyDistance)
-                {
-                    int jp = j + butterflySize;
-                    double tempRe =
-                        outputRe[jp] * wRe - outputIm[jp] * wIm;
-                    double tempIm =
-                        outputRe[jp] * wIm + outputIm[jp] * wRe;
-                    outputRe[jp] = outputRe[j] - tempRe;
-                    outputIm[jp] = outputIm[j] - tempIm;
-                    outputRe[j] += tempRe;
-                    outputIm[j] += tempIm;
-                }
-                double tempWRe = wRe * uRe - wIm * uIm;
-                double tempWIm = wRe * uIm + wIm * uRe;
-                wRe = tempWRe;
-                wIm = tempWIm;
-            }
-        }
-    }
-
-    // ビットを左右反転した配列を返す
-    private static int[] BitScrollArray(int arraySize)
+    int length)
     {
-        int[] reBitArray = new int[arraySize];
-        int arraySizeHarf = arraySize >> 1;
+        int i, j, k, n, m, r, stage, number_of_stage;
+        double a_real, a_imag, b_real, b_imag, c_real, c_imag, real, imag;
 
-        reBitArray[0] = 0;
-        for (int i = 1; i < arraySize; i <<= 1)
+        number_of_stage = length; // 呼び出し時の値
+        int dataSize = 1 << number_of_stage; // データ数
+        int N = inR.Length;
+
+        outputRe = new double[N];
+        outputIm = new double[N];
+
+        // バタフライ計算
+        for (stage = 1; stage <= number_of_stage; stage++)
         {
-            for (int j = 0; j < i; j++)
-                reBitArray[j + i] = reBitArray[j] + arraySizeHarf;
-            arraySizeHarf >>= 1;
+            for (i = 0; i < Math.Pow(2,stage - 1); i++)
+            {
+                for (j = 0; j < Math.Pow(2,number_of_stage - stage); j++)
+                {
+                    n = (int)Math.Pow(2,number_of_stage - stage + 1) * i + j;
+                    m = (int)Math.Pow(2,number_of_stage - stage) + n;
+                    r = (int)Math.Pow(2, stage - 1) * j;
+                    a_real = inR[n];
+                    a_imag = inI[n];
+                    b_real = inR[m];
+                    b_imag = inR[m];
+                    c_real = Math.Cos((2.0 * Math.PI * r) / N);
+                    c_imag = -Math.Sin((2.0 * Math.PI * r) / N);
+                    if (stage < number_of_stage)
+                    {
+                        outputRe[n] = a_real + b_real;
+                        outputIm[n] = a_imag + b_imag;
+                        outputRe[m] = (a_real - b_real) * c_real - (a_imag - b_imag) * c_imag;
+                        outputIm[m] = (a_imag - b_imag) * c_real + (a_real - b_real) * c_imag;
+                    }
+                    else
+                    {
+                        outputRe[n] = a_real + b_real;
+                        outputIm[n] = a_imag + b_imag;
+                        outputRe[m] = a_real - b_real;
+                        outputIm[m] = a_imag - b_imag;
+                    }
+                }
+            }
         }
-        return reBitArray;
+
+        // インデックスの並び替えのためのテーブルの作成
+        int[] index = new int[N];
+        for (stage = 1; stage <= number_of_stage; stage++)
+        {
+            for (i = 0; i < Math.Pow(2,stage - 1); i++)
+            {
+                index[(int)Math.Pow(2, stage - 1) + i] = 
+                    index[i] + (int)Math.Pow(2,number_of_stage - stage);
+            }
+        }
+
+        // インデックスの並び替え
+        for (k = 0; k < N; k++)
+        {
+            if (index[k] > k)
+            {
+                real = outputRe[index[k]];
+                imag = outputIm[index[k]];
+                outputRe[index[k]] = outputRe[k];
+                outputIm[index[k]] = outputIm[k];
+                outputRe[k] = real;
+                outputIm[k] = imag;
+            }
+        }
     }
+
+
+    public static void IFFT(
+       double[] inR,
+       double[] inI,
+       out int[] outputRe,
+       out int[] outputIm,
+       int length,
+       int waveLength)
+    {
+        int number_of_stage = length; // 呼び出し時の値
+        int i, j, k, n, m, r, stage;
+        double a_real, a_imag, b_real, b_imag, c_real, c_imag, real, imag;
+        int N = waveLength; // 出力波のサイズ
+
+        outputRe = new int[N];
+        outputIm = new int[N];
+
+        /* バタフライ計算 */
+        for (stage = 1; stage <= number_of_stage; stage++)
+        {
+            for (i = 0; i < Math.Pow(2,stage - 1); i++)
+            {
+                for (j = 0; j < Math.Pow(2, number_of_stage - stage); j++)
+                {
+                    n = (int)Math.Pow(2, number_of_stage - stage + 1) * i + j;
+                    m = (int)Math.Pow(2, number_of_stage - stage) + n;
+                    r = (int)Math.Pow(2, stage - 1) * j;
+                    a_real = inR[n];
+                    a_imag = inI[n];
+                    b_real = inR[m];
+                    b_imag = inR[m];
+                    c_real = Math.Cos((2.0 * Math.PI * r) / N);
+                    c_imag = Math.Sin((2.0 * Math.PI * r) / N);
+                    if (stage < number_of_stage)
+                    {
+                        outputRe[n] = (int)(a_real + b_real);
+                        outputIm[n] = (int)(a_imag + b_imag);
+                        outputRe[m] = (int)((a_real - b_real) * c_real - (a_imag - b_imag) * c_imag );
+                        outputIm[m] = (int)((a_imag - b_imag) * c_real + (a_real - b_real) * c_imag );
+                    }
+                    else
+                    {
+                        outputRe[n] = (int)(a_real + b_real);
+                        outputIm[n] = (int)(a_imag + b_imag);
+                        outputRe[m] = (int)(a_real - b_real);
+                        outputIm[m] = (int)(a_imag - b_imag);
+                    }
+                }
+            }
+        }
+
+        /* インデックスの並び替えのためのテーブルの作成 */
+        int[] index = new int[N];
+        for (stage = 1; stage <= number_of_stage; stage++)
+        {
+            for (i = 0; i < Math.Pow(2,stage - 1); i++)
+            {
+                index[(int)Math.Pow(2, stage - 1) + i]
+                    = index[i] + (int)Math.Pow(2, number_of_stage - stage);
+            }
+        }
+
+        /* インデックスの並び替え */
+        for (k = 0; k < N; k++)
+        {
+            if (index[k] > k)
+            {
+                real = outputRe[index[k]];
+                imag = outputIm[index[k]];
+                outputRe[index[k]] = outputRe[k];
+                outputIm[index[k]] = outputIm[k];
+                outputRe[k] = (int)real;
+                outputIm[k] = (int)imag;
+            }
+        }
+
+        // 計算結果をNで割る
+        for (k = 0; k < N; k++)
+        {
+            // InR.Length とかで割っちゃうと，音が消滅する
+            // ( 元のfft.hの N = 入力配列の要素数 )
+            outputRe[k] /= 1;
+            outputIm[k] /= 1;
+
+            // 逆フーリエ変換後の虚数領域の扱いどうするんだろ？
+            outputRe[k] += outputIm[k];
+            outputRe[k] /= 2;
+        }
+
+        int[] temp = outputRe;
+        int[] temp2 = outputIm;
+    }
+
 
     // 終了時に呼ぶべき処理(メモ)
     private void End___()
@@ -302,5 +367,16 @@ class ShowSound : Node
 
         // ソフトウエアサウンドプレイヤーハンドルを削除
         DX.DeleteSoftSoundPlayer(SoftSoundPlayerHandle);
+    }
+
+
+
+    private void makeText()
+    {
+        // テキストデータの生成
+        letter = new Letter();
+        letter.Color = DX.GetColor(127, 127, 127);
+        letter.LocalPos = new Vect(400, 400);
+        AddChild(letter);
     }
 }
