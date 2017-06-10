ef1ae31ab00e27eaa1db49fb588de333869c3ec3
elipmoc <relipmoc101@gmail.com>
2017-06-11 00:41:12 +0900

    おーとまとんそとがわだけかんせい

---
+++ dxlibex/dxlibex/Base/Automaton/SemanticAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXEX.Base
+{
+    //セマンティックアクション。意味は分からんが、かっこいいからこの名前を付けた
+    public class SemanticAction<Key>
+    {
+        public void Do(Rule<Key> rule)
+        {
+        }
+    }
+}
